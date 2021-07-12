fb575c88c9b281da91e777281f1b559cf00a2e6e
Regis Zaleman <regis.zaleman@gmail.com>
2021-07-12 12:09:14 -0400

    feat(frontend): :sparkles: Create the dashboard header component
    Barebones, will need to be designed/branded

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import FormsList from './components/FormsList'
 import FormEditor from './components/FormEditor'
 import NewFormButton from './components/NewFormButton'
+import BrandingHeader from './components/Dashboard/BrandingHeader'
 // import FormSettings from './components/FormEditor/FormSettings'
 
 import './App.css'
@@ -12,6 +13,7 @@ import './App.css'
 const App: React.FC = () => (
   <>
     <div id="sat-app" className="container mx-auto px-8 my-8 mx-w-3xl">
+      <BrandingHeader />
       <div className="flex flex-row justify-between items-baseline mb-6 mt-8">
         <h1 className="text-3xl font-extralight">
           Forms
---
+++ frontend/src/components/Dashboard/BrandingHeader.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const BrandingHeader: React.FC = () => {
+  return (
+    <div className="h-24 bg-gray-300 flex flex-row items-center p-4 rounded-md">
+      <h1 className="text-4xl">Show &amp; Tell</h1>
+    </div>
+  )
+}
+
+export default BrandingHeader
